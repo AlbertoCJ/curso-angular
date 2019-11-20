22e24104bf4b28e0b4f9eb0c9c20e0a851221fac
albertoCJ <albertocj88@gmail.com>
2019-11-20 20:42:49 +0100

    12-mapas

---
+++ 12-mapas/src/app/classes/marcador.class.ts
@@ -0,0 +1,8 @@
+export class Marcador {
+    constructor( public lat: number, 
+                 public lng: number,
+                 public titulo: string = 'Sin título',
+                 public desc: string = 'Sin descripción' ) {
+
+    }
+}
