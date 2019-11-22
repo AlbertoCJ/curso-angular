f4adfd53b49cb298bdd329af77b732360452dace
albertoCJ <albertocj88@gmail.com>
2019-11-22 19:45:36 +0100

    16-graficos

---
+++ 16-graficos/src/app/components/dona/dona.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styleUrls: ['./dona.component.css']
+})
+export class DonaComponent implements OnInit {
+
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public numeros_random() {
+
+    const data: any = [];
+    for (let i = 0; i < 3; i++) {
+      data[i] = [this.randomNumber(), this.randomNumber(), this.randomNumber()];
+    }
+    this.doughnutChartData = data;
+  }
+
+  private randomNumber() {
+    return Math.round(Math.random() * 100);
+  }
+
+}
